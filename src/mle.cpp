3b6d51653ffa8a3980dd64a17ada8828c02e9bc6
teng-gao <gaoteng@wustl.edu>
2022-07-02 17:20:12 -0400

    mle works

--- src/mle.cpp
+++ src/mle.cpp
@@ -25,17 +25,19 @@ public:
 
         std::vector<double> l = l_lnpois_cpp(Y_obs, lambda_ref, d, mu, sig);
 
-        return l[1];
+        return -l[0];
     };
 };
 
 // [[Rcpp::export]]
 void fit_lnpois_cpp() {
   fit_lnpois model;
-  Roptim<fit_lnpois> opt("BFGS");
+  Roptim<fit_lnpois> opt("L-BFGS-B");
   opt.control.trace = 1;
   opt.set_hessian(true);
-  
+  arma::vec lower = {-arma::datum::inf, 0.01};
+  opt.set_lower(lower);
+
   arma::vec x = {0, 1};
   opt.minimize(model, x);
   
